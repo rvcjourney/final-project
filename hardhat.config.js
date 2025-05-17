a2e579f8dfcaadee2cb1a86b73b85b78b6eca18a
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-05-17 13:10:41 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,34 @@
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.9",
+//   networks: {
+//     hardhat: {
+//       chainId: 1337,
+//     },
+//   },
+//   paths: {
+//     artifacts: "./client/src/artifacts",
+//   },
+// };
+
+
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.9",
+//   networks: {
+//     hardhat: {
+//       chainId: 1337,
+//     },
+//   },
+//   paths: {
+//     artifacts: "./client/src/artifacts",
+//   },
+// };
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -5,10 +36,14 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
-      chainId: 1337,
+      chainId: 31337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", // Default Hardhat local node RPC
+      chainId: 31337, // Custom chain ID
     },
   },
   paths: {
     artifacts: "./client/src/artifacts",
-  },
+  },
 };
